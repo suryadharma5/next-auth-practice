8da6d610c396976c4faa19766a6c4705263abab2
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-08-01 22:54:14 +0700

    feat: add register UI

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,4 +6,11 @@ export const LoginSchema = z.object({
     password: z.string().min(1, "Password is required")
 })
 
+export const RegisterSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6, "Minumum 6 characters required"),
+    name: z.string().min(1, "Name is required")
+})
+
 export type TLoginSchema = z.infer<typeof LoginSchema>
+export type TRegisterSchema = z.infer<typeof RegisterSchema>
