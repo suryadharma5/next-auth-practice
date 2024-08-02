f3a3405b4de202556776001fab80b3b14039c6b3
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-08-02 21:59:13 +0700

    feat: add session and token callbacks

--- auth.ts
+++ auth.ts
@@ -1,12 +1,57 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, UserRole } from "@prisma/client"
 import authConfig from "./auth.config"
 import { prismaClient } from "./lib/db"
+import { getUserById } from "./data/user"
  
-const prisma = new PrismaClient()
- 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const {
+  auth,
+  handlers,
+  signIn,
+  signOut
+} = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   console.log({signIn: user})
+
+    //   const existingUser = await getUserById(user.id ?? "")
+
+    //   if(!existingUser || !existingUser.emailVerified) {
+    //     return false
+    //   }
+
+    //   return true
+    // },
+
+    async session({ token, session }) {
+      console.log({
+        sessionToken: token,
+        session: session
+      })
+
+      if(session.user && token.sub && token.role) {
+        session.user.id = token.sub
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+    async jwt({ token }) {
+      console.log({token})
+
+      if(!token.sub) {
+        return token
+      }
+
+      const existingUser = await getUserById(token.sub)
+
+      if(!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(prismaClient),
   session: { strategy: "jwt" },
   ...authConfig,
