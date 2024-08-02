2256bcadeb4a4e4744468fb635748b93a3681686
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-08-02 19:37:44 +0700

    feat: helper function for mapping public route

---
+++ route.ts
@@ -0,0 +1,32 @@
+/**
+ * An array of routes that are accesible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = [
+    "/"
+]
+
+
+/**
+ * An array of routes that are used for authentication
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register"
+]
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purpose
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * The default redirect path after login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
